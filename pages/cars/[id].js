50d816e68e8ce31f001e30aa6b4f167834bc6609
Taylor Masiello <masiellotay@gmail.com>
2021-06-10 00:59:34 -0700

    Complete

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -18,9 +18,7 @@ export default function Car({ car }) {
   );
 }
 
-
-
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
   const req = await fetch(`http://localhost:3000/${params.id}.json`);
   const data = await req.json();
 
@@ -29,16 +27,25 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export async function getStaticPaths() {
-  const req = await fetch("http://localhost:3000/cars.json");
-  const data = await req.json();
-
-  const paths = data.map((car) => {
-    return { params: { id: car } };
-  });
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//export async function getStaticProps({ params }) {
+//  const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//  const data = await req.json();
+//
+//  return {
+//    props: { car: data },
+//  };
+//}
+//
+//export async function getStaticPaths() {
+//  const req = await fetch("http://localhost:3000/cars.json");
+//  const data = await req.json();
+//
+//  const paths = data.map((car) => {
+//    return { params: { id: car } };
+//  });
+//
+//  return {
+//    paths,
+//    fallback: false,
+//  };
+//}
